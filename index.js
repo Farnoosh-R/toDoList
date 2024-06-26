251ad016c3af8b4f826d448c4783502c2ec2b90e
Morteza Soltanpour <morteza.cs@gmail.com>
2024-06-26 14:49:07 +0330

    remove items

--- index.js
+++ index.js
@@ -7,138 +7,157 @@ let elOutputList3 = document.getElementById("outputList3");
 let elOutputList4 = document.getElementById("outputList4");
 
 let arrayList = [];
-const arrayPriList = ['high', 'middle', 'low'];
-
+const arrayPriList = ["high", "middle", "low"];
 
 // window.onload = function saveData() {
-   
+
 //     localStorage.setItem("toDoList", JSON.stringify(arrayList));
 //     alert( localStorage.getItem('toDoList') );
 //   };
 
-function getCat(){
-    let catEl = document.getElementById("cat");
-    let catElText = 
-    catEl.options[catEl.selectedIndex].text;
-    return catElText;
-}
-function getPeriority(){
-    let perEl = document.getElementById("periority");
-    let perElText = 
-    perEl.options[perEl.selectedIndex].value;
-    return perElText;
-}
-function getPrintPriTitle(priValue){
-    return arrayPriList[priValue - 1];
-    
+function getCat() {
+  let catEl = document.getElementById("cat");
+  let catElText = catEl.options[catEl.selectedIndex].text;
+  return catElText;
 }
 
-function sortObjectsByBoolean(arr) {
-    arr.sort((a, b) => a.isDone - b.isDone);
-    
+function getCatId() {
+  let catEl = document.getElementById("cat");
+  let catElValue = catEl.options[catEl.selectedIndex].value;
+  return catElValue;
 }
 
-function toDoList(){
-    let id = uuid();
- 
-    let elOutputList = document.createElement('div');
-    elOutputList.setAttribute('id', `divoutput_${id}`);
-    let elDiv = document.createElement('div');
-    elDiv.setAttribute('id', `div_${id}`);
-    let elCat = document.createElement('label');
-    elCat.setAttribute('id', `cat_${id}`);
-    let elPer = document.createElement('label');
-    elPer.setAttribute('id', `per_${id}`);
-    let elLabel = document.createElement('label');
-    elLabel.setAttribute('id', `label_${id}`);
-    elLabel.setAttribute('for', `checkbox_${id}`);
-    elLabel.classList.add('labelItem');
-    let i = document.createElement('i');
-    i.classList.add("fa", "fa-trash-o");
-    i.addEventListener('click', ()=> trash(id));
-    let checkBox = document.createElement('input');
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.setAttribute('id', `checkbox_${id}`);
-    checkBox.classList.add('checkbox');
-    checkBox.addEventListener('change', ()=> done(id));
-      
+function getPeriority() {
+  let perEl = document.getElementById("periority");
+  let perElText = perEl.options[perEl.selectedIndex].value;
+  return perElText;
+}
+function getPrintPriTitle(priValue) {
+  return arrayPriList[priValue - 1];
+}
 
-    arrayList.push({
-        title: elList.value,
-        cat: getCat(),
-        priority: getPeriority(),
-        index: 0,
-        isDone: false,
-        trash : false,
-        id: id
-    });
-  
-    
-    elLabel.innerHTML  = elList.value;
-    elCat.innerHTML = getCat();
-    elPer.innerHTML = getPrintPriTitle(getPeriority());
-    elDiv.appendChild(checkBox);
-    elDiv.appendChild(elCat);
-    elDiv.appendChild(elLabel);
-    elDiv.appendChild(elPer);
-    elDiv.appendChild(i);
-    elOutputList.appendChild(elDiv);
-    if (getCat() == 'خرید'){
-        elOutputList1.appendChild(elOutputList);
-    } else if (getCat() == 'پزشکی'){
-        elOutputList2.appendChild(elOutputList);
-    }else if (getCat() == 'وقت'){
-        elOutputList3.appendChild(elOutputList);
-    }else if (getCat() == 'برنامه در صف اجرا'){
-        elOutputList4.appendChild(elOutputList);
-    }
+function sortObjectsByBoolean() {
+  arrayList.sort((a, b) => a.isDone - b.isDone);
+}
 
-    if (getPeriority() == '1'){
+function createTask(id) {
+  var item = arrayList.filter((p) => p.id == id)[0];
 
-        elPer.classList.add('per-h');
-    }else if(getPeriority() == '2'){
-        elPer.classList.add('per-m');
-    }else if(getPeriority() == '3'){
-        elPer.classList.add('per-l');
-    }
-    
-    elList.value = '';
+  let elOutputList = document.createElement("div");
+  elOutputList.setAttribute("id", `divoutput_${id}`);
+  let elDiv = document.createElement("div");
+  elDiv.setAttribute("id", `div_${id}`);
+  let elCat = document.createElement("label");
+  elCat.setAttribute("id", `cat_${id}`);
+  let elPer = document.createElement("label");
+  elPer.setAttribute("id", `per_${id}`);
+  let elLabel = document.createElement("label");
+  elLabel.setAttribute("id", `label_${id}`);
+  elLabel.setAttribute("for", `checkbox_${id}`);
+  elLabel.classList.add("labelItem");
+  let i = document.createElement("i");
+  i.classList.add("fa", "fa-trash-o");
+  i.addEventListener("click", () => trash(id));
+  let checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("id", `checkbox_${id}`);
+  checkBox.classList.add("checkbox");
+  checkBox.addEventListener("change", () => done(id));
 
+  if (item.isDone) {
+    elDiv.classList.add("done");
+    checkBox.checked = true;
+  }
+
+  elLabel.innerHTML = item.title;
+  elCat.innerHTML = item.cat;
+  elPer.innerHTML = getPrintPriTitle(item.priority);
+  elDiv.appendChild(checkBox);
+  elDiv.appendChild(elCat);
+  elDiv.appendChild(elLabel);
+  elDiv.appendChild(elPer);
+  elDiv.appendChild(i);
+  elOutputList.appendChild(elDiv);
+
+  if (getPeriority() == "1") {
+    elPer.classList.add("per-h");
+  } else if (getPeriority() == "2") {
+    elPer.classList.add("per-m");
+  } else if (getPeriority() == "3") {
+    elPer.classList.add("per-l");
+  }
+
+  return elOutputList;
+}
+function toDoList() {
+  let id = uuid();
+
+  arrayList.push({
+    title: elList.value,
+    cat: getCat(),
+    catId: getCatId(),
+    priority: getPeriority(),
+    index: 0,
+    isDone: false,
+    trash: false,
+    id: id,
+  });
+
+  displayItems();
+
+  elList.value = "";
 }
 function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
-function trash(id){
-    let e = document.getElementById(`divoutput_${id}`);
-    let t = document.getElementById(`div_${id}`);
-    
-    let trashx = arrayList.filter(c => c.id === id)
-    trashx[0].trash = true;
-    e.removeChild(t);
-    
+function trash(id) {
+  let e = document.getElementById(`divoutput_${id}`);
+  let t = document.getElementById(`div_${id}`);
+
+  let trashx = arrayList.filter((c) => c.id === id);
+  trashx[0].trash = true;
+  e.removeChild(t);
 }
-function done(id){
-    let e = document.getElementById(`divoutput_${id}`);
-    let d = document.getElementById(`div_${id}`);
-    let c = document.getElementById(`checkbox_${id}`);
-    let donex = arrayList.filter(c => c.id === id)
+function done(id) {
+  let e = document.getElementById(`divoutput_${id}`);
+  let d = document.getElementById(`div_${id}`);
+  let c = document.getElementById(`checkbox_${id}`);
+  let donex = arrayList.filter((c) => c.id === id);
 
-    if (c.checked){
-        donex[0].isDone = true;
-        d.classList.add('done');
-       
-    }else{
-        donex[0].isDone = false;
-        d.classList.remove('done');
-    }
-    sortObjectsByBoolean(arrayList);
+  if (c.checked) {
+    donex[0].isDone = true;
+    d.classList.add("done");
+  } else {
+    donex[0].isDone = false;
+    d.classList.remove("done");
+  }
 
-    //e.innerHTML = "";
-    
+  displayItems();
 }
 
+function displayItems() {
+  clearAll();
+  sortObjectsByBoolean();
+  AddItemToPage();
+}
 
+function clearAll() {
+  document.getElementById("outputList1").innerHTML = "";
+  document.getElementById("outputList2").innerHTML = "";
+  document.getElementById("outputList3").innerHTML = "";
+  document.getElementById("outputList4").innerHTML = "";
+}
+
+function AddItemToPage() {
+  arrayList.forEach((item, index) => {
+    var elOutputList = createTask(item.id);
+
+    var outputList = document.getElementById(item.catId);
+    outputList.appendChild(elOutputList);
+  });
+}
